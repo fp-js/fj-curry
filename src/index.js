34cd49b89b727501f091221cacb2dc75b1e3faf6
Eric Churchill <eric.churchill2@gmail.com>
2017-01-06 18:03:41 -0800

    move new files to source directory

---
+++ src/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const _curry = (arity, fn, curryArgs = []) => {
+  return (...arg) => {
+    var concatArgs = [...curryArgs, ...arg];
+
+    // If current function arity greater than number of received args
+    if (arity > concatArgs.length) {
+      return _curry(arity, fn, concatArgs);
+    } else {
+      return fn(...concatArgs);
+    }
+  };
+};
+
+export const curry = (fn) => _curry(fn.length, fn);
+
+export const curryN = (n, fn) => _curry(n, fn);
+
+export const curry1 = curryN(1, curryN)(1);
+export const curry2 = curryN(2, curryN)(2);
+export const curry3 = curryN(3, curryN)(3);
+export const curry4 = curryN(4, curryN)(4);
