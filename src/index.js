e1f6af4f606bcc7b50e2f23037ecbbff5815810e
Eric Churchill <eric.churchill2@gmail.com>
2017-01-06 22:03:02 -0800

    rewrite tests for mocha and delete gulpfile

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 const _curry = (arity, fn, curryArgs = []) => {
   return (...arg) => {
     var concatArgs = [...curryArgs, ...arg];
-
     // If current function arity greater than number of received args
     if (arity > concatArgs.length) {
       return _curry(arity, fn, concatArgs);
@@ -17,7 +16,7 @@ export const curry = (fn) => _curry(fn.length, fn);
 
 export const curryN = (n, fn) => _curry(n, fn);
 
-export const curry1 = curryN(1, curryN)(1);
+export const curry1 = curryN(2, curryN)(1);
 export const curry2 = curryN(2, curryN)(2);
-export const curry3 = curryN(3, curryN)(3);
-export const curry4 = curryN(4, curryN)(4);
+export const curry3 = curryN(2, curryN)(3);
+export const curry4 = curryN(2, curryN)(4);
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,5 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
 import { spy } from 'sinon';
 import {
   curry,
@@ -9,83 +10,129 @@ import {
   curry4
 } from './index.js';
 
+chai.use(sinonChai);
+
 describe('fj-curry', () => {
+
+  let spiedCallback;
+  beforeEach(() => {
+    spiedCallback = spy();
+  });
+  const a1 = (w) => spiedCallback();
+  const a2 = (w, x) => spiedCallback();
+  const a3 = (w, x, y) => spiedCallback();
+  const a4 = (w, x, y, z) => spiedCallback();
 
   describe('curry', () => {
 
     it('should exist', () => {
       expect(curry).to.be.ok;
     });
-    //   t.plan(5);
-    //
-    //   let spy = stub().returns(true);
-    //   function func(a, b, c) {
-    //     return spy.apply(this, arguments);
-    //   }
-    //
-    //   t.equal(typeof curry, 'function');
-    //
-    //   t.equal(curry(func)(1, 2)(3, 4), true);
-    //   t.ok(spy.calledWith(1, 2, 3));
-    //
-    //   t.equal(curry(func)(1)(2, 3), true);
-    //   t.ok(spy.calledWith(1, 2, 3));
-    // });
+
+    it('should return a function', () => {
+      expect(curry(x => x)).to.be.a('function');
+    });
+
+    it('should accept functions of arbitrary arities', () => {
+      expect(curry(a1)).to.be.a('function');
+      expect(curry(a2)).to.be.a('function');
+      expect(curry(a3)).to.be.a('function');
+    });
+
+    it('should delay invocation of curried functions', () => {
+      const lift1 = curry(a1);
+      const lift2 = curry(a2);
+      const lift3 = curry(a3);
+
+      lift2(null);
+      lift3(null, null);
+      lift3(null)(null);
+      expect(spiedCallback).to.have.not.been.called;
+
+      lift1(null);
+      expect(spiedCallback).to.have.been.calledOnce;
+      lift2(null);
+      expect(spiedCallback).to.have.been.calledOnce;
+      lift2(null)(null);
+      expect(spiedCallback).to.have.been.calledTwice;
+      lift3(null, null);
+      expect(spiedCallback).to.have.been.calledTwice;
+      lift3(null, null)(null);
+      expect(spiedCallback).to.have.been.calledThrice;
+      lift3(null)(null)(null);
+      expect(spiedCallback).to.have.callCount(4);
+      lift3(null, null, null);
+      expect(spiedCallback).to.have.callCount(5);
+    });
   });
 
-  // it('fj-curry#curryN', (t) => {
-  //   t.plan(7);
-  //
-  //   let spy = stub().returns(true);
-  //
-  //   t.equal(typeof curryN, 'function');
-  //   t.equal(curryN(1, spy)(1, 2), true);
-  //   t.ok(spy.calledWith(1));
-  //
-  //   t.equal(curryN(3, spy)(1, 2)(3, 4), true);
-  //   t.ok(spy.calledWith(1, 2, 3));
-  //
-  //   t.equal(curryN(3, spy)(1)(2, 3), true);
-  //   t.ok(spy.calledWith(1, 2, 3));
-  // });
-  //
-  // test('fj-curry#curry1', (t) => {
-  //   t.plan(3);
-  //
-  //   let spy = stub().returns(true);
-  //
-  //   t.equal(typeof curry1, 'function');
-  //   t.equal(curry1(spy)(1, 2), true);
-  //   t.ok(spy.calledWith(1));
-  // });
-  //
-  // test('fj-curry#curry2', (t) => {
-  //   t.plan(3);
-  //
-  //   let spy = stub().returns(true);
-  //
-  //   t.equal(typeof curry2, 'function');
-  //   t.equal(curry2(spy)(1)(2, 3), true);
-  //   t.ok(spy.calledWith(1, 2));
-  // });
-  //
-  // test('fj-curry#curry3', (t) => {
-  //   t.plan(3);
-  //
-  //   let spy = stub().returns(true);
-  //
-  //   t.equal(typeof curry3, 'function');
-  //   t.equal(curry3(spy)(1)(2)(3, 4), true);
-  //   t.ok(spy.calledWith(1, 2, 3));
-  // });
-  //
-  // test('fj-curry#curry4', (t) => {
-  //   t.plan(3);
-  //
-  //   let spy = stub().returns(true);
-  //
-  //   t.equal(typeof curry4, 'function');
-  //   t.equal(curry4(spy)(1)(2)(3)(4, 5), true);
-  //   t.ok(spy.calledWith(1, 2, 3, 4));
-  // });
-})
+  describe('curryN', () => {
+
+    it('should accept two arguments', () => {
+      expect(curryN.length).to.equal(2);
+    });
+
+    it('should be curriable', () => {
+      expect(curry(curryN(1))).to.be.a('function');
+    });
+
+    it('should accept N of arbitrary size', () => {
+      const lift1 = curryN(1, a1);
+      const lift2 = curryN(2, a2);
+      const lift3 = curryN(3, a3);
+
+      lift1(1);
+      lift2(2)(3);
+      lift3(4)(5)(6);
+
+      expect(spiedCallback).to.have.been.calledThrice;
+    });
+  })
+
+  describe('curry1', () => {
+
+    it('should invoke curried function after passed single argument', () => {
+      const lift1 = curry1(a1);
+      lift1(null);
+
+      expect(spiedCallback).to.have.been.calledOnce;
+    });
+  });
+
+  describe('curry2', () => {
+
+    it('should invoke curried function after passed two arguments', () => {
+      const lift2 = curry2(a2);
+      lift2(null, null);
+
+      expect(spiedCallback).to.have.been.calledOnce;
+    });
+  });
+
+  describe('curry3', () => {
+
+    it('should invoke curried function after passed three arguments', () => {
+      const lift3 = curry3(a3);
+      lift3(null, null, null);
+
+      expect(spiedCallback).to.have.been.calledOnce;
+    });
+  });
+
+  describe('curry4', () => {
+
+    it('should invoke curried function after passed four arguments', () => {
+      const lift4 = curry4(a4);
+      lift4(null, null, null, null);
+
+      expect(spiedCallback).to.have.been.calledOnce;
+    });
+
+    it('should invoke curried function irrespective of its arity', () => {
+      const lift4 = curry4(a1);
+      lift4(null)(null)(null)(null);
+
+      expect(spiedCallback).to.have.been.calledOnce;
+    });
+  });
+});
